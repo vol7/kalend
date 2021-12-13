40406c2365b4e5b928910f21f61a962a1af6b920
nibdo <hello@nibdo.com>
2021-12-13 23:08:20 +0100

    Add tests for allDay events funcs

--- src/test/utils/dates.test.ts
+++ src/test/utils/dates.test.ts
@@ -5,6 +5,7 @@ import {
   isEventInRange,
 } from '../../utils/eventLayout';
 
+import { TEST_TIMEZONE } from '../common';
 import assert from 'assert';
 
 const baseDate = '2021-11-07';
@@ -16,7 +17,7 @@ const event1Data: any = {
   calendarID: '1',
   startAt: `${baseDate}T18:00:00.000Z`,
   endAt: `${baseDate}T19:00:00.000Z`,
-  timezoneStartAt: 'Europe/Berlin',
+  timezoneStartAt: TEST_TIMEZONE,
 };
 const event2Data: any = {
   id: '2',
@@ -25,7 +26,7 @@ const event2Data: any = {
   calendarID: '1',
   startAt: `${baseDate}T20:00:00.000Z`,
   endAt: `${baseDate}T21:00:00.000Z`,
-  timezoneStartAt: 'Europe/Berlin',
+  timezoneStartAt: TEST_TIMEZONE,
 };
 const event3Data: any = {
   id: '3',
@@ -34,15 +35,21 @@ const event3Data: any = {
   calendarID: '1',
   startAt: `${baseDate}T05:00:00.000Z`,
   endAt: `${baseDate}T22:00:00.000Z`,
-  timezoneStartAt: 'Europe/Berlin',
+  timezoneStartAt: TEST_TIMEZONE,
 };
 
 describe(`dates funcs`, function () {
   it('checkOverlappingEvents func: Should find no overlapping', function () {
-    assert.equal(checkOverlappingEvents(event1Data, event2Data), false);
+    assert.equal(
+      checkOverlappingEvents(event1Data, event2Data, TEST_TIMEZONE),
+      false
+    );
   });
   it('checkOverlappingEvents func: Should find overlapping', function () {
-    assert.equal(checkOverlappingEvents(event1Data, event3Data), true);
+    assert.equal(
+      checkOverlappingEvents(event1Data, event3Data, TEST_TIMEZONE),
+      true
+    );
   });
   it('checkOverlappingDatesForHeaderEvents func: Should find no overlapping', function () {
     assert.equal(
@@ -52,7 +59,8 @@ describe(`dates funcs`, function () {
           startAt: '2021-11-07T05:00:00.000Z',
           endAt: '2021-11-07T08:00:00.000Z',
         },
-        DateTime.fromISO('2021-11-08T05:00:00.000Z')
+        DateTime.fromISO('2021-11-08T05:00:00.000Z'),
+        TEST_TIMEZONE
       ),
       false
     );
@@ -65,7 +73,8 @@ describe(`dates funcs`, function () {
           startAt: '2021-11-07T05:00:00.000Z',
           endAt: '2021-11-07T08:00:00.000Z',
         },
-        DateTime.fromISO('2021-11-07T05:00:00.000Z')
+        DateTime.fromISO('2021-11-07T05:00:00.000Z'),
+        TEST_TIMEZONE
       ),
       true
     );
@@ -82,7 +91,8 @@ describe(`dates funcs`, function () {
             startAt: '2021-11-06T00:00:00.000Z',
             endAt: '2021-11-07T00:00:00.000Z',
           },
-          DateTime.fromISO('2021-11-07T03:00:00.000Z')
+          DateTime.fromISO('2021-11-07T03:00:00.000Z'),
+          TEST_TIMEZONE
         ),
         false
       );
@@ -100,7 +110,8 @@ describe(`dates funcs`, function () {
             startAt: '2021-11-06T00:00:00.000Z',
             endAt: '2021-11-07T00:00:00.000Z',
           },
-          DateTime.fromISO('2021-11-06T03:00:00.000Z')
+          DateTime.fromISO('2021-11-06T03:00:00.000Z'),
+          TEST_TIMEZONE
         ),
         true
       );
@@ -118,7 +129,8 @@ describe(`dates funcs`, function () {
         [
           DateTime.fromISO('2021-11-06T03:00:00.000Z'),
           DateTime.fromISO('2021-11-05T03:00:00.000Z'),
-        ]
+        ],
+        TEST_TIMEZONE
       ),
       true
     );
@@ -135,7 +147,8 @@ describe(`dates funcs`, function () {
         [
           DateTime.fromISO('2021-11-02T03:00:00.000Z'),
           DateTime.fromISO('2021-11-03T03:00:00.000Z'),
-        ]
+        ],
+        TEST_TIMEZONE
       ),
       false
     );
